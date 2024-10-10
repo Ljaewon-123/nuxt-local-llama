698718a5d634681310361892d87bd2c84082c090
Ljaewon-123 <jml6388@naver.com>
2024-10-10 20:45:00 +0900

    채팅에 마크다운 적용
    문제 채팅이 끝날때 까지 모든 엘리먼트 업데이트됨 불필요한 동작이라 판단됨

--- nuxt-llama-gpt/app/components/chat/Llama.vue
+++ nuxt-llama-gpt/app/components/chat/Llama.vue
@@ -14,10 +14,8 @@
     <div v-if="loading" >
       <LazyLoading :justify="'start'" />
     </div>
-    <div class="answer">
-      {{ answer }}
-    </div>
-
+    <div class="answer" v-html="parsedAnswer"></div>
+    <!-- <div class="answer">{{ answer }}</div> -->
     <div>
       some options area
     </div>
@@ -26,14 +24,19 @@
 </template>
 
 <script setup lang="ts">
+import { marked } from 'marked';
+
 const props = defineProps({
   coment: String
 })
 const { $socket }  = useNuxtApp();
 
 const answer = ref("")
+const parsedAnswer = ref();
+
 watchEffect(() => {
   answer.value += props.coment ?? ''
+  parsedAnswer.value = marked(answer.value); // 마크다운을 HTML로 변환
 })
 const loading = ref(true)
 
